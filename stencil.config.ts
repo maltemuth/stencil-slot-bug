6b7cf374ab0cb148541ee878b3120bc81aade7f0
Malte Muth <malte.muth@experienceone.com>
2019-09-09 13:52:49 +0200

    add bug example

---
+++ stencil.config.ts
@@ -0,0 +1,19 @@
+import {Config} from '@stencil/core';
+
+export const config: Config = {
+  namespace: 'slot-bug',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers,
+      baseUrl: 'http://example.org/',
+    },
+  ],
+};
